15f9b9b266468f99c61c84fcbc4b3724ead72313
Sergio Nunez <c.hecho.28@hotmail.com>
2019-10-24 10:11:23 -0500

    avance laboratorio

---
+++ src/main/java/edu/eci/arep/main/SaveAppApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.SaveApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/eci/arep/persistences/UsuarioPersistence.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arep.persistences;
+
+import edu.application.Persistence.Persistences;
+import edu.application.model.Usuario;
+import edu.application.Persistence.impl.RouletteDB;
+import edu.application.Exceptions.RoulettePersistenceException;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author 2115253
+ */
+@Service
+public class UsuarioPersistence {
+    
+    RouletteDB rb = new RouletteDB(); 
+    
+    @Override
+    public void realizaConexion() throws RoulettePersistenceException {
+        rb.realizaConexion();
+    }
+
+    public void insertarUsuario(Usuario user) {
+        rb.insertarUsuario(user);
+    }
+
+    
+    public Usuario getUsuario(String user) throws RoulettePersistenceException {
+        return rb.getUsuario(user);
+    }
+
+    public void updateUsuario(Usuario us){
+        rb.updateUsuario(us);
+    }
+    
+}
